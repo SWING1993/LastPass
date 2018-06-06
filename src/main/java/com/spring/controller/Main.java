936c5b4ce246d99f53c8f9bdea6c3d367f776da1
SWING <songguohua10446627@gmail.com>
2018-06-06 18:17:35 +0800

    update

--- src/main/java/com/spring/controller/Main.java
+++ src/main/java/com/spring/controller/Main.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-public class main {
+public class Main {
 
     @RequestMapping("/")
     @ResponseBody
